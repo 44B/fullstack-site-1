ae3b167c5648a930e0b53d60c72976389ac658d0
mohur <m.ashrahfi@gmail.com>
2020-05-11 17:54:32 -0700

    setting up react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        Dashboard Component
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <Route path="/" component={ExpenseDashboardPage} /> 
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
