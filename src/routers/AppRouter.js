754ed56fb4097c82fcfd0b7bd8c1496d4311f166
mohur <m.ashrahfi@gmail.com>
2020-05-12 20:31:29 -0700

    organizing routers pt.2

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
-import AddExpensePage from './AddExpensePage';
-import EditExpensePage from './EditExpensePage';
-import ExpenseDashboardPage from './ExpenseDashboardPage';
-import HelpPage from './HelpPage';
-import NotFoundPage from './NotFoundPage';
-
-
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
-);
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
 
 const AppRouter = () => (
     <BrowserRouter>
