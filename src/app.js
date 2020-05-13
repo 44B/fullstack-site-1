4b727c2afa2ed0523a4005bd47e4348d4e863495
mohur <m.ashrahfi@gmail.com>
2020-05-12 19:42:25 -0700

    organizing routes

--- src/app.js
+++ src/app.js
@@ -1,62 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        Dashboard Component
-    </div>
-);
 
-const AddExpensePage = () => (
-    <div>
-        Add Expense Component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        Edit Expense Component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        Help Component
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404 Not Found - <Link to="/">Home</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <Link to="/">Dashboard</Link>
-        <Link to="/create">Add Expense</Link>
-        <Link to="/edit">Edit Expense</Link>
-        <Link to="/help">Help</Link>
-    </header>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} />
-                <Route path="/create" component={AddExpensePage} /> 
-                <Route path="/edit" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));    
---
+++ src/routers/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>
+        Edit Expense Component
+    </div>
+);
+
+export default EditExpensePage;
