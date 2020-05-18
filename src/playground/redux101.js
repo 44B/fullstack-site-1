bfe9ac4250762af4fe1599d22bbb492446b390a3
mohur <m.ashrahfi@gmail.com>
2020-05-18 04:50:04 -0700

    dispatching generic actions for redux

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,30 +1,44 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
+const store = createStore((state = {count: 10}, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy 
             };
         case 'RESET':
             return {
                 count: 0
+            };
+        case 'SET':
+            return {
+                count: action.count
             };
         default: 
             return state;
     }
 });
 
-store.dispatch({
-    type: 'INCREMENT'
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
 });
 
 store.dispatch({
-    type: 'DECREMENT'
+    type: 'INCREMENT',
+    incrementBy: 7
+});
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 5
 });
 
 store.dispatch({
@@ -36,7 +50,8 @@ store.dispatch({
 });
 
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'SET',
+    count: 101
 });
 
-console.log(store.getState());
+unsubscribe();
