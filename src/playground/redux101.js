5ba5258cbd81070c5a6de2920a0ae650ae8f8ff5
mohur <m.ashrahfi@gmail.com>
2020-05-20 05:40:30 -0700

    refactoring w/ action generators

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,57 +1,60 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = {count: 10}, action) => {
+const incrementCount = ({ incrementBy = 1 }) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 }) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({ count }) => ({
+    type: 'SET',
+    count
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
+
+const store = createStore((state = {count: 0}, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - decrementBy 
-            };
-        case 'RESET':
-            return {
-                count: 0
+                count: state.count - action.decrementBy 
             };
         case 'SET':
             return {
                 count: action.count
+            };
+        case 'RESET':
+            return {
+                count: 0
             };
         default: 
             return state;
     }
 });
 
-console.log(store.getState());
-
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 });
 
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 7
-});
+store.dispatch(incrementCount({ incrementBy: 2 }));
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 5
-});
+store.dispatch(incrementCount({ incrementBy: 10 }));
 
-store.dispatch({
-    type: 'DECREMENT'
-});
+store.dispatch(decrementCount({ decrementBy: 5 }));
 
-store.dispatch({
-    type: 'RESET'
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
+store.dispatch(setCount({ count: 100 }));
 
 unsubscribe();
