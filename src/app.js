7c6b90ad4b852619b54dfb969b79da17e4ce12ac
mohur <m.ashrahfi@gmail.com>
2020-06-06 17:04:24 -0700

    Outputting list items from redux

--- src/app.js
+++ src/app.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import configureStore from '../src/store/configureStore'
+import configureStore from '../src/store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './selectors/expenses';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
 const store = configureStore();
+
+
+store.dispatch(addExpense({ description: 'Water Bill' }));
+store.dispatch(addExpense({ description: 'Gas Bill' }));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('Rent'));
+}, 3000)
+
+
 
 const jsx = (
     <Provider store={store}>
