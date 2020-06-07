47b272c16add58dab2730892f7fe01a6a294ca1e
mohur <m.ashrahfi@gmail.com>
2020-06-07 14:31:19 -0700

    rendering individual expenses

--- src/app.js
+++ src/app.js
@@ -12,14 +12,16 @@ import './styles/styles.scss';
 const store = configureStore();
 
 
-store.dispatch(addExpense({ description: 'Water Bill' }));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 5500 }));
 store.dispatch(addExpense({ description: 'Gas Bill' }));
 
 setTimeout(() => {
     store.dispatch(setTextFilter('rent'));
 }, 3000)
 
-
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
