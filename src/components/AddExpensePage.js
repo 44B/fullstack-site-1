cccba91a98ce1c41adeb8d7c0537408bf1cc95fb
mohur <m.ashrahfi@gmail.com>
2020-06-11 13:54:20 -0700

    adding data to redux store and history.push()

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ExpenseForm from './ExpenseForm'
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
     <div>
         Add Expense Component
         <ExpenseForm 
         onSubmit={(expense) => {
-            console.log(expense);
+            props.dispatch(addExpense(expense));
+            props.history.push('/');
         }}
         />
     </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
