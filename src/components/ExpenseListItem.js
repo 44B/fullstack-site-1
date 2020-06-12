b3d23234df0a85d4e3fd87b1a5ed9e4e1f72f345
mohur <m.ashrahfi@gmail.com>
2020-06-12 16:56:07 -0700

    removing listitem in editexpense

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, id, dispatch, createdAt }) => (
+const ExpenseListItem = ({ description, amount, id, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
         <p> {amount} - {createdAt} </p>
-        <button onClick={() => {
-        dispatch(removeExpense({ id }));    
-    }}>Remove</button>
     </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
